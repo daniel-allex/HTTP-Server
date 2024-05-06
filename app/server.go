534e65e8bfb3622cdac7437b031626c33419ba01
root <root@LAPTOP-G4RJJCES>
2024-05-05 22:07:07 -0400

    Wrote OK message

--- app/server.go
+++ app/server.go
@@ -6,6 +6,19 @@ import (
 	"os"
 )
 
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// Read and process data from the client
+	// ...
+
+	// Write data back to the client
+	// ...
+
+	message := "HTTP/1.1 200 OK\r\n\r\n"
+	conn.Write([]byte(message))
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -18,9 +31,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleClient(conn)
 }
