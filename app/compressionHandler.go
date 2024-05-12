6107e85d776ded3201e0a1f253cf5d072888291e
root <root@LAPTOP-G4RJJCES>
2024-05-12 16:53:09 -0400

    Added multiple compression types

--- app/compressionHandler.go
+++ app/compressionHandler.go
@@ -1,9 +1,15 @@
 package main
 
+import "strings"
+
 func (builder *HTTPResponseBuilder) compress(request *HTTPMessage) *HTTPResponseBuilder {
-	if compressType, ok := request.headers["accept-encoding"]; ok {
-		if compressType == "gzip" {
-			return builder.setHeader("content-encoding", compressType)
+	if val, ok := request.headers["accept-encoding"]; ok {
+		compressTypes := strings.Split(val, ", ")
+
+		for _, compressType := range compressTypes {
+			if compressType == "gzip" {
+				return builder.setHeader("content-encoding", compressType)
+			}
 		}
 	}
 
